ee96a95dee5b7d5f6e54a44cd553f7672223307e
Domen <domen.gnezda@pm.me>
2023-03-14 11:16:01 +0100

    Update trees.py
    Add functions remove_child() and traverse()

--- trees.py
+++ trees.py
@@ -1,4 +1,5 @@
 class TreeNode:
+    
     def __init__(self, value):
         self.value = value
         self.children = []
@@ -6,12 +7,29 @@ class TreeNode:
     def __repr__(self, level=0):
         # HELPER METHOD TO PRINT TREE!
         ret = "--->" * level + repr(self.value) + "\n"
+        
         for child in self.children:
             ret += child.__repr__(level+1)
+        
         return ret
 
     def add_child(self, child_node):
         self.children.append(child_node) 
+
+    def remove_child(self, child_node):
+        print("Removing " + child_node.value + " from " + self.value)
+        self.children = [child for child in self.children 
+                        if child is not child_node]
+
+    def traverse(self):
+        print("Traversing...")
+        nodes_to_visit = [self]
+
+        while len(nodes_to_visit) > 0:
+            current_node = nodes_to_visit.pop()
+            print(current_node.value)
+            nodes_to_visit += current_node.children
+
 
 ### TEST CODE TO PRINT TREE
 company = [
